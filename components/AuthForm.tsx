f3cdf0b69811dcbf3b575f2811a208e0d493fe59
Pushpendra7409 <pushpendra7409@gmail.com>
2024-06-19 17:10:22 +0530

    Sign Up page complete

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,29 +19,55 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import CustomInput from './CustomInput';
-import { authformSchema } from '@/lib/utils';
+import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import {getLoggedInUser, signIn} from '@/lib/actions/user.actions'
+import {signUp} from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setisLoading] = useState(false);
 
+  const formSchema = authFormSchema(type);
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof authformSchema>>({
-    resolver: zodResolver(authformSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: '',
       password: '',
     },
-  });
+  })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authformSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setisLoading(true)
-    console.log(values)
-    setisLoading(false);
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setisLoading(true);
+    try {
+      // sign up with Appwrite & create plaid token
+      if(type === 'sign-up') {
+         const newUser = await signUp(data);
+        setUser(newUser);
+      }
+      
+      if(type === 'sign-in') {
+        //  const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // })
+
+        //  if (response) router.push ('/')
+      
+      }
+
+    } 
+    catch (error)  {
+      console.log(error)
+    } finally {
+      setisLoading (false);
+    }
+    
   }
 
   return (
@@ -74,8 +100,27 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {type === 'sign-up' && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                    <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name' />
+                  </div>
+                  <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your address' />
+                  <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='state' label="State" placeholder='Example: Uttar Pradesh' />
+                    <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput control={form.control} name='dateOfBirth' label="Date of Birth" placeholder='YYYY-MM-DD' />
+                    <CustomInput control={form.control} name='mob' label="Mobile.No" placeholder='Example:10-12 digit Mob number' />
+                  </div>
+                </>
+              )}
             <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
-            <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
+            <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" 
+            />
 
             <div className="flex flex-col gap-4">
         <Button type="submit" disabled={isLoading} className='form-btn'>{isLoading ? (
--- lib/utils.ts
+++ lib/utils.ts
@@ -196,7 +196,17 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authformSchema = z.object({
+export const authFormSchema = (type: string) => z.object({
+  // sign up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(50),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  mob: type === 'sign-in' ? z.string().optional() : z.string().min(10).max(12),
+  // both
   email: z.string().email(),
   password: z.string().min(8),
 })
