c7cb4b4a664667d6758e96fdb4e96c22767161ac
Pushpendra7409 <pushpendra7409@gmail.com>
2024-06-22 12:47:21 +0530

    make a login and sign up dynamic and sentry install

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <main>{children}</main>
+      <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+      <div className="auth-asset">
+       <div>
+        <Image 
+        src='/icons/auth-image.jpg'
+        alt='Auth image'
+        width={800}
+        height={900}
+        />
+       </div>
+      </div>
+      </main>
     );
   }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,16 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react'
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIn = { firstName: 'Pushpendra', lastName:'Singh', email:'pushpendr7409@gmail.com'};
+
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className='home'>
        <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox type='greeting'  title='Welcome' user={loggedIn?.firstName || 'Guest'}
+          <HeaderBox type='greeting'  title='Welcome' user={loggedIn?.name || 'Guest'}
           subtext='Access and manage your account and transactions efficiently.'
           />
 
